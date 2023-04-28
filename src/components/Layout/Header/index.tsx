17999b0e467630eea4c95baf2203b452621abd91
memokey <skytopground@gmail.com>
2023-04-28 14:24:47 -0700

    changed

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -22,18 +22,20 @@ const Header = () => {
     <div className="fixed top-0 left-0 right-0 bg-[#141414] z-[100]">
         <div
             className="sm:flex xs:hidden
-                        custom-2xl:px-[56px] xl:px-[25px] lg:px-[56px] md:px-[25px] sm:px-[20px] xs:px-[24px]
-                        custom-2xl:flex-row xl:flex-row lg:flex-col md:flex-col sm:flex-col
-                        justify-between
-                        border-b-[1px] border-semiSplitter
-                        custom-2xl:h-[92px] xl:h-[92px] lg:h-[144px] md:h-[144px] sm:h-[144px] xs:h-[92px]
-                        w-full"
+              custom-2xl:px-[56px] xl:px-[25px] lg:px-[56px] md:px-[25px] sm:px-[20px] xs:px-[24px]
+              custom-2xl:flex-row xl:flex-row lg:flex-col md:flex-col sm:flex-col
+              justify-between
+              border-b-[1px] border-semiSplitter
+              custom-2xl:h-[92px] xl:h-[92px] lg:h-[144px] md:h-[144px] sm:h-[144px] xs:h-[92px]
+              w-full
+            "
         >
             <div className="flex">
                 <LogoComp />
                 <div
                     className="flex flex-row h-full
-                                lg:justify-between md:justify-around sm:justify-between xs:justify-between"
+                      lg:justify-between md:justify-around sm:justify-between xs:justify-between
+                    "
                 >
                     {HeaderMenuTitles.map((menu: any, index) => (
                         <HeaderMenuItem
